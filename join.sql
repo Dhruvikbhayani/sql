71736c1f64885bfcfc7738c7b0014a6ed345ff86
Dhruvik Bhayani <92158308+Dhruvikbhayani@users.noreply.github.com>
2022-02-15 13:56:17 +0530

    15-02

--- join.sql
+++ join.sql
@@ -1,56 +1,56 @@
 -- 1] Find out the products, which have been sold to 'Ivan Bayross'.
-select pm.Description
+select Client_Master.clientName,pm.Description
 from Client_Master
 inner join Sales_Order so on Client_Master.ClientNo = so.ClientNo
-inner join Sales_Order_Details sod on so.OrderNo = sod.OrderNo
+inner join sales_order_detail sod on so.OrderNo = sod.OrderNo
 inner join Product_Master pm on sod.ProductNo = pm.ProductNo
-where Client_Master.Name = 'Ivan Baross'
+where Client_Master.clientname= 'Ivan Baross'
 
 -- 2] Find out the products and their quantities that will have to be delivered in the current month.
-select Client_Master.Name,
+select Client_Master.clientname,
 	pm.Description,
 	so.DelyDate,
-	sod.Qty_Ordered
+	sod.qtyorderd
 from Client_Master
 inner join Sales_Order so on Client_Master.ClientNo = so.ClientNo
-inner join Sales_Order_Details sod on so.OrderNo = sod.OrderNo
+inner join sales_order_detail sod on so.OrderNo = sod.OrderNo
 inner join Product_Master pm on sod.ProductNo = pm.ProductNo
 where so.DelyDate like '%-07-%'
 
 -- 4]Find the names of clients who have purchased 'Trousers'.
-select Client_Master.Name,
+select Client_Master.clientname,
 	pm.Description
 from Client_Master
 inner join Sales_Order so on so.ClientNo = Client_Master.ClientNo
-INNER JOIN Sales_Order_Details sod on so.OrderNo = sod.OrderNo
+INNER JOIN sales_order_detail sod on so.OrderNo = sod.OrderNo
 inner join Product_Master pm on sod.ProductNo = pm.ProductNo
-where pm.Description = 'Trousers'
+where pm.Description = 'shirts'
 
 -- 5] List the products and orders from cusromers who have ordered less than 5 units of 'Pull Overs'.
-select Client_Master.Name,
+select Client_Master.clientname,
 	pm.Description
 from Client_Master
 inner join Sales_Order so on so.ClientNo = Client_Master.ClientNo
-INNER JOIN Sales_Order_Details sod on so.OrderNo = sod.OrderNo
+INNER JOIN sales_order_detail sod on so.OrderNo = sod.OrderNo
 inner join Product_Master pm on sod.ProductNo = pm.ProductNo
-where pm.Description = 'Pull Over' AND sod.Qty_Ordered < 5
+where pm.Description = 'Pull Over' AND sod.qtyorderd < 5
 
 -- 6] Find out the products and their quaintities for the orders placed by 'Ivan Bayross' and 'Mamta Muzumdar'.
-select Client_Master.Name,
+select Client_Master.clientname,
 	pm.Description,
-	sod.Qty_Ordered
+	sod.qtyorderd
 from Client_Master
 inner join Sales_Order so on so.ClientNo = Client_Master.ClientNo
-INNER JOIN Sales_Order_Details sod on so.OrderNo = sod.OrderNo
+INNER JOIN sales_order_detail sod on so.OrderNo = sod.OrderNo
 inner join Product_Master pm on sod.ProductNo = pm.ProductNo
-where Client_Master.Name = 'Ivan Baross' OR Client_Master.Name = 'Mamta Muzumdar'
+where Client_Master.clientName = 'Ivan Baross' OR Client_Master.clientName = 'Mamta Muzumdar'
 
 -- 7] Find out the products and their quaintities for the orders placed by ClientNo 'C00001' and 'C00003'.
-select Client_Master.Name,
+select Client_Master.clientname,
 	pm.Description,
-	sod.Qty_Ordered
+	sod.qtyorderd
 from Client_Master
 inner join Sales_Order so on so.ClientNo = Client_Master.ClientNo
-INNER JOIN Sales_Order_Details sod on so.OrderNo = sod.OrderNo
+INNER JOIN sales_order_detail sod on so.OrderNo = sod.OrderNo
 inner join Product_Master pm on sod.ProductNo = pm.ProductNo
 where Client_Master.ClientNo = 'C00001' OR Client_Master.ClientNo = 'C00003'
